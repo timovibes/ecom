5b0c8d596acc0530f6a666b4434e9b54dc608a1a
timovibes <timothyator744@gmail.com>
2026-08-03 17:39:01 +0300

    change port from 73 to 74

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Ecommerce API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
