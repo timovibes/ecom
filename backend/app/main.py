8c802411f0f12d575ec8ebfc76b83f84dbe46079
timovibes <timothyator744@gmail.com>
2026-08-30 10:00:00 +0300

    change port to 5174

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Ecommerce API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
